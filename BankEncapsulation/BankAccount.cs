1e4f8d6e00c1404d97da8d8c380843771a539270
Anastasia higgins <higgins.ana@gmail.com>
2023-05-01 17:46:37 -0400

    first commit

---
+++ BankEncapsulation/BankAccount.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankEncapsulation
+{
+    internal class BankAccount
+    {
+        private double balance = 0;
+
+        public void Deposit(double amount)
+        {
+            balance += amount;
+        }
+
+        public double GetBalance()
+        {
+            return balance;
+        }
+    }
+    //In your BankAccount class, create the following:
+
+    //    A private field of type double named balance with a value of 0
+    //    Define a method named Deposit that will accept a double and store that value in the balance field
+    //    Define a method named GetBalance that will return the amount stored in the balance field
+
+
+}
--- BankEncapsulation/Program.cs
+++ BankEncapsulation/Program.cs
@@ -4,7 +4,17 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            BankAccount account = new BankAccount();
+
+            Console.WriteLine("Enter your deposit amount:");
+            account.Deposit(Convert.ToDouble(Console.ReadLine()));
+
+            account.GetBalance();
+            Console.WriteLine("Your balance is:");
+            Console.WriteLine(account.GetBalance());
+
+            //In the main method of your application, create a new instance of the BankAccount class.
+            //Allow the user of the application to Deposit money and retrieve their balance through the console.
         }
     }
 }
